e924f243cc5780cfae5e42f05e77bee9d61311fe
thiago perrig <thiago perrig>
2026-06-24 11:59:56 -0300

    primer commit

---
+++ src/main/java/com/billeteradigital/BilleteraDigitalApplication.java
@@ -0,0 +1,12 @@
+package com.billeteradigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleteraDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilleteraDigitalApplication.class, args);
+    }
+}
